ba9b8cd66eb4634404a382514d6c06ff18c72262
KDW03 <asasa0628@gs.cwnu.ac.kr>
2024-05-26 16:59:00 +0900

    build : implemented build-logic build.gradle #1

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    `kotlin-dsl`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+}
