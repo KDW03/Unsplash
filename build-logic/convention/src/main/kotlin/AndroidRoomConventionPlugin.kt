0afd1b35b5785720d2c598a97134124c67981447
KDW03 <asasa0628@gs.cwnu.ac.kr>
2024-05-26 17:10:15 +0900

    feat : Implemented conventionPlugins #1

---
+++ build-logic/convention/src/main/kotlin/AndroidRoomConventionPlugin.kt
@@ -0,0 +1,18 @@
+import com.example.swing.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidRoomConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply("com.google.devtools.ksp")
+
+            dependencies {
+                add("implementation", libs.findLibrary("room.runtime").get())
+                add("implementation", libs.findLibrary("room.ktx").get())
+                add("ksp", libs.findLibrary("room.compiler").get())
+            }
+        }
+    }
+}
