d2eef1a7ebdc68c9b5430e1551da117f95e1e94d
KDW03 <asasa0628@gs.cwnu.ac.kr>
2024-05-27 23:50:51 +0900

    feat : dataStore proto 구현

---
+++ core/datastore/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("swing.android.library")
+    id("swing.android.hilt")
+    alias(libs.plugins.protobuf)
+}
+
+android {
+    defaultConfig {
+        consumerProguardFiles("consumer-proguard-rules.pro")
+    }
+    namespace = "com.example.swing.core.datastore"
+}
+
+protobuf {
+    protoc {
+        artifact = libs.protobuf.protoc.get().toString()
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                register("java") {
+                    option("lite")
+                }
+                register("kotlin") {
+                    option("lite")
+                }
+            }
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":core:model"))
+    implementation(project(":core:common"))
+    implementation(libs.androidx.dataStore.core)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.protobuf.kotlin.lite)
+}
